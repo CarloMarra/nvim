2c0d5769f0dfcdf39f002808e58dac2a9c95d149
CarloMarra <carlo.marra18199@gmail.com>
2026-06-18 14:46:20 +0200

    Update configuration

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
   {
-    "navarasu/onedark.nvim",
-    opts = {
-      style = "darker", -- dark | darker | cool | deep | warm | warmer | light
-    },
+    "sainnhe/everforest",
+    priority = 1000,
+    init = function()
+      vim.o.background = "dark"
+      vim.g.everforest_background = "medium" -- soft | medium | hard
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "everforest",
     },
   },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
     picker = {
       sources = {
         explorer = {
+          ignored = true,
           layout = {
             layout = {
               position = "right",
@@ -16,4 +17,11 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+
+    vim.api.nvim_set_hl(0, "SnacksPickerPathIgnored", {
+      fg = "#808080",
+    })
+  end,
 }
